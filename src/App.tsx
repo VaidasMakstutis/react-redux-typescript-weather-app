16f46127faa21a1fe43c646ffe944d6c03cf8389
Vaidas Makstutis <vaidas.makstutis@gmail.com>
2022-07-29 13:22:25 +0300

    h2 element

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,13 @@ import React, { FC } from "react";
 import "./App.css";
 
 const App: FC = () => {
-  return <div className="App">Hello World!</div>;
+  return (
+    <>
+      <div className="App">
+        <h2>Hello World!</h2>
+      </div>
+    </>
+  );
 };
 
 export default App;
